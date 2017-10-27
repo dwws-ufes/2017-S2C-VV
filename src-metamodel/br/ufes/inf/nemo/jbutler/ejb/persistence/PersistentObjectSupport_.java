88a86a62075d8f6dbcb52f2cd6154ec2fa8befa0
Marco Brunoro <ma.brunoro@gmail.com>
2017-10-27 09:52:18 -0200

    updated wildfly to 11 final version

--- src-metamodel/br/ufes/inf/nemo/jbutler/ejb/persistence/PersistentObjectSupport_.java
+++ src-metamodel/br/ufes/inf/nemo/jbutler/ejb/persistence/PersistentObjectSupport_.java
@@ -4,9 +4,9 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2017-10-25T18:17:03.093-0200")
+@Generated(value="Dali", date="2017-10-27T09:42:48.196-0200")
 @StaticMetamodel(PersistentObjectSupport.class)
 public class PersistentObjectSupport_ {
-	public static volatile SingularAttribute<PersistentObjectSupport, Long> id;
-	public static volatile SingularAttribute<PersistentObjectSupport, Long> version;
+	public static volatile SingularAttribute<PersistentObjectSupport, Object> id;
+	public static volatile SingularAttribute<PersistentObjectSupport, Object> version;
 }
